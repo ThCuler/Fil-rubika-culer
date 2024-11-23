d4e344c5496d3baf5573a7e1d1810e3e98b1da55
ThCuler <pes2023.23.24.25@gmail.com>
2024-11-23 07:08:07 +0330

    Update Culer.py

--- Culer.py
+++ Culer.py
@@ -77,7 +77,7 @@ if code=="1":print("(https://cigarettes-chemical-rid-numeric.trycloudflare.com  
 if code=="2":print("https://github.com/farnamryson/filtering-robika/issues/28--------------------http://dxprit-mehrab-sigari-28-hacker.phpnet.us---------------https://m.me11r.com/albums/338266/danni-rivers-gets-her-tight-pussy-drilled-in-various-positions/----------------https://github.com/aware18/mr-dxpirt-filtering.rubika.sex/blob/main/index.html")
 if code=="3":print("https://theintercept.com/2022/12/09/iran-regime-khamenei-death---------‐--https://xgore.net/category/bestgore/dead-female/--------------------https://l.zxvax.com/2132.html")
 if code=="4":print("https://photo.sex.com/5.3.8.4/g/y///v//w/s//5.2.4.7.2.8.6.2.7.9.3.7.1.5.7.3.7.8.1.6.3.6.2.8.2com")
-if code=="5":print("")
+if code=="5":print("https://www.pornhub.com/view_video.php?viewkey=ph62541e492c3a7------------------https://www.pornhub.com/interstitial?viewkey=ph6203e4bf1b1d9")
 if code=="6":print("http://xa.fltvuq.cn/?account.Code=10749999999172-Shervi.Gazme.zan-hackers65.78766.infect.in.29*offs.ec/2Mcezwr")
 if code=="7":print("v.3.1.1.yftt15k.in")
 if code=="8":print("bug_AI = bug-9.9.9-support {<https://rubika.ir/SupportBot>} ip : 80.249.115.8 tcp {<https://rubika.ir/Netreport>} log account {<report-user.AI>} AI-report : Spam%100")
