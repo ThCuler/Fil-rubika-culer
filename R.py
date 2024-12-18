f4589f7283b290c35522fc35184a646d9f7aed31
ThCuler <pes2023.23.24.25@gmail.com>
2024-12-18 20:17:44 +0330

    Update R.py

--- R.py
+++ R.py
@@ -1,3 +1,5 @@
 import random as r
 Lest = ['https://github.com/farnamryson/filtering-robika/issues/28','http://dxprit-mehrab-sigari-28-hacker.phpnet.us','https://m.me11r.com/albums/338266/danni-rivers-gets-her-tight-pussy-drilled-in-various-positions/','https://github.com/aware18/mr-dxpirt-filtering.rubika.sex/blob/main/index.html']
 Lest1 = ['188382881818289292881919298383838']
+print(r.choice(Lest))
+print(r.choice(Lest1))
